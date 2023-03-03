5aa2aeac62720c15d3d9cd9388d7b6aaf7655c3a
Mazer <macbookpro@Mazer.local>
2023-03-03 11:41:47 +0700

    udpate

--- src/com/kimly/test.groovy
+++ src/com/kimly/test.groovy
@@ -1,7 +1,17 @@
 package com.kimly
 
 def testingMethod(){
-    println(">>> testing Method")
+    int me = 5;
+    if (me==5){
+        println(">>> me is 5")
+    }else{
+        println(">>> me isn't 5")
+    }
+    println(">>> testing for loop")
+    for(int a = 1;a < 5;a++){
+        println(a)
+    }
+
 }
 
 return this
