b1098e89239fcdc87dd861097719f84f8d852153
Mazer <macbookpro@Mazer.local>
2023-03-03 09:50:27 +0700

    add new method

--- vars/myVar.groovy
+++ vars/myVar.groovy
@@ -3,3 +3,11 @@ def call(){
    println("this is "+test+".")
    sh "echo Hello World, This is me"
 }
+
+def sum(){
+   int a = 3;
+   float b = 8.4;
+   
+   println(a+b)
+   sh "echo this is my sum"
+}
