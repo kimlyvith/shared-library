5d8cdaef1b64944886893f8cbc552fc0b044815b
Mazer <macbookpro@Mazer.local>
2023-03-03 11:09:00 +0700

    update

--- vars/loadMyScript.groovy
+++ vars/loadMyScript.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]){
     def scriptcontents = libraryResource "com/kimly/script/${config.name}";
+    println(">>>>>>." +scriptcontents)
     writeFile file: "${config.name}", text: scriptcontents
     sh "chmod a+x ./${config.name}"
 }
