8d53de2023252d2a2fe29fb326482dd52a4f3b51
Mazer <macbookpro@Mazer.local>
2023-03-03 11:49:48 +0700

    test call class

---
+++ src/com/kimly/core.groovy
@@ -0,0 +1,16 @@
+package com.kimly
+
+static def DisplayName() {
+      println("This is how methods work in groovy");
+      println("This is an example of a simple method");
+   } 
+	
+static void main(String[] args) {
+      DisplayName();
+   } 
+}
+// class Person{
+//     int age = 20;
+//     float height = 1.75;
+//     String color = "white"
+// }
