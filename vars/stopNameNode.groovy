b395365bbef710ca3c551d81580b20e2833cb4d1
Kimly Vith <kimlyvithkimly@gmail.com>
2023-03-21 10:15:42 +0700

    Delete stopNameNode.groovy

--- vars/stopNameNode.groovy
+++
@@ -1,6 +0,0 @@
-def call(Map config = [:]){
-    def scriptcontents = libraryResource "com/kimly/script/${config.name}";
-    println(">>>>>>." +scriptcontents)
-    writeFile file: "${config.name}", text: scriptcontents
-    sh "chmod a+x ./${config.name}"
-}
